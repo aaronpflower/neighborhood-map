4b98955b81eec003b2b11302f9acbc18bc10bd0a
Aaron Flower <aaronpflower@gmail.com>
2015-10-15 16:10:30 -0600

    basic UI setup

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+	app = express(),
+	port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+	res.render('index')
+});
+
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var x = server.address().port;
+
+	console.log('Neighborhood map is running on localhost:', host, x);
+});
+
+app.set('views', __dirname + '../../views');
+app.set('view engine', 'jade');
+app.use('/client', express.static(__dirname + '/../../client'))
+app.use('/client', express.static(__dirname + '/../../client/img'))
