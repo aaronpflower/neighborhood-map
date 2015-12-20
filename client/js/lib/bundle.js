a8e1aad9a788070536bbea2c50354f3a342d9130
Aaron Flower <aaronpflower@gmail.com>
2015-12-19 20:07:48 -0700

    wip

--- client/js/lib/bundle.js
+++ client/js/lib/bundle.js
@@ -554,86 +554,86 @@ var weddingDetails = {
                             boulderMap,
                         ]),
                     ]),
-                    m("div", {class: "second-section"}, [
+                    m("div.second-section", [
                         m("div.about-us", [
-                            m("div", {class: "about-us-item"}, [
+                            m("div.about-us-item", [
                                 m("img", {src: "../../../client/img/amanda.jpg"}),
                                 m("div.overlay", [
                                     m("h1", "Gettin married right after this"),
                                 ]),
                             ]),
-                            m("div", {class: "about-us-item"}, [
-                                m("div.about-us-text", [
+                            m("divabout-us-item", [
+                                m("div.about-party", [
                                     m("h1", "Main of Honor: Amanda"),
                                     m("p", "Aaron Paul Flower was born on beautiful spring day in the bustling metropolis of Holland, MI to loving parents Paul and Lori Flower. Babyhood was Aaron’s jam, and he enjoyed his frequent naps and early bedtimes. Aaron’s childhood and adolescence was marked by a natural athleticism, an admiration for snow, and an affinity for curmudgeonly behavior. It is said in Flower family lore that Aaron spent 78% of his spare time with his arms crossed in protest, which aided in the coining of his nickname “Grumpy Bunny.”"),
                                 ])
                             ])
                         ]),
                         m("div#aaron.about-us", [
-                            m("div", {class: "about-us-item"}, [
+                            m("div.about-us-item", [
                                 m("img", {src: "../../../client/img/ryan.jpg"}),
                                 m("div.overlay", [
                                     m("h1", "Straight outta Monte Vista"),
                                 ]),
                             ]),
-                            m("div", {class: "about-us-item"}, [
-                                m("div.about-us-text", [
+                            m("div.about-us-item", [
+                                m("div.about-party", [
                                     m("h1", "Best Man: Ryan"),
                                     m("p", "Aaron Paul Flower was born on beautiful spring day in the bustling metropolis of Holland, MI to loving parents Paul and Lori Flower. Babyhood was Aaron’s jam, and he enjoyed his frequent naps and early bedtimes. Aaron’s childhood and adolescence was marked by a natural athleticism, an admiration for snow, and an affinity for curmudgeonly behavior. It is said in Flower family lore that Aaron spent 78% of his spare time with his arms crossed in protest, which aided in the coining of his nickname “Grumpy Bunny.”"),
                                 ])
                             ])
                         ]),
                         m("div.about-us", [
-                            m("div", {class: "about-us-item"}, [
+                            m("div.about-us-item", [
                                 m("img", {src: "../../../client/img/bailey.jpg"}),
                                 m("div.overlay", [
                                     m("h1", "Gonna be a momma"),
                                 ]),
                             ]),
-                            m("div", {class: "about-us-item"}, [
-                                m("div.about-us-text", [
+                            m("div.about-us-item", [
+                                m("div.about-party", [
                                     m("h1", "Bridesmaid: Bailey"),
                                     m("p", "Aaron Paul Flower was born on beautiful spring day in the bustling metropolis of Holland, MI to loving parents Paul and Lori Flower. Babyhood was Aaron’s jam, and he enjoyed his frequent naps and early bedtimes. Aaron’s childhood and adolescence was marked by a natural athleticism, an admiration for snow, and an affinity for curmudgeonly behavior. It is said in Flower family lore that Aaron spent 78% of his spare time with his arms crossed in protest, which aided in the coining of his nickname “Grumpy Bunny.”"),
                                 ])
                             ])
                         ]),
                         m("div#aaron.about-us", [
-                            m("div", {class: "about-us-item"}, [
+                            m("div.about-us-item", [
                                 m("img", {src: "../../../client/img/ty.jpg"}),
                                 m("div.overlay", [
                                     m("h1", "Mucho take it easy"),
                                 ]),
                             ]),
-                            m("div", {class: "about-us-item"}, [
-                                m("div.about-us-text", [
+                            m("div.about-us-item", [
+                                m("div.about-party", [
                                     m("h1", "Groomsman: Ty"),
                                     m("p", "Aaron Paul Flower was born on beautiful spring day in the bustling metropolis of Holland, MI to loving parents Paul and Lori Flower. Babyhood was Aaron’s jam, and he enjoyed his frequent naps and early bedtimes. Aaron’s childhood and adolescence was marked by a natural athleticism, an admiration for snow, and an affinity for curmudgeonly behavior. It is said in Flower family lore that Aaron spent 78% of his spare time with his arms crossed in protest, which aided in the coining of his nickname “Grumpy Bunny.”"),
                                 ])
                             ])
                         ]),
                         m("div.about-us", [
-                            m("div", {class: "about-us-item"}, [
+                            m("div.about-us-item", [
                                 m("img", {src: "../../../client/img/aracely.jpg"}),
                                 m("div.overlay", [
                                     m("h1", "Weeeee!"),
                                 ]),
                             ]),
-                            m("div", {class: "about-us-item"}, [
-                                m("div.about-us-text", [
+                            m("div.about-us-item", [
+                                m("div.about-party", [
                                     m("h1", "Bridesmaid: Aracely"),
                                     m("p", "Aaron Paul Flower was born on beautiful spring day in the bustling metropolis of Holland, MI to loving parents Paul and Lori Flower. Babyhood was Aaron’s jam, and he enjoyed his frequent naps and early bedtimes. Aaron’s childhood and adolescence was marked by a natural athleticism, an admiration for snow, and an affinity for curmudgeonly behavior. It is said in Flower family lore that Aaron spent 78% of his spare time with his arms crossed in protest, which aided in the coining of his nickname “Grumpy Bunny.”"),
                                 ])
                             ])
                         ]),
                         m("div#aaron.about-us", [
-                            m("div", {class: "about-us-item"}, [
+                            m("div.about-us-item", [
                                 m("img", {src: "../../../client/img/chase.jpg"}),
                                 m("div.overlay", [
                                     m("h1", "Hazaah!"),
                                 ]),
                             ]),
-                            m("div", {class: "about-us-item"}, [
-                                m("div.about-us-text", [
+                            m("div.about-us-item", [
+                                m("div.about-party", [
                                     m("h1", "Groomsman: Chase"),
                                     m("p", "Aaron Paul Flower was born on beautiful spring day in the bustling metropolis of Holland, MI to loving parents Paul and Lori Flower. Babyhood was Aaron’s jam, and he enjoyed his frequent naps and early bedtimes. Aaron’s childhood and adolescence was marked by a natural athleticism, an admiration for snow, and an affinity for curmudgeonly behavior. It is said in Flower family lore that Aaron spent 78% of his spare time with his arms crossed in protest, which aided in the coining of his nickname “Grumpy Bunny.”"),
                                 ])
