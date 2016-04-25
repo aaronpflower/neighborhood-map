202db536be473a600bfbba20964ffcf3bac9acee
Aaron Flower <aaronpflower@gmail.com>
2016-04-24 19:51:02 -0600

    test

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ var server = app.listen(port, function() {
 	console.log('Wedding Site is running on localhost', host, x);
 });
 
-app.set('views', 'src/views');
+app.set('views', 'server/src/views');
 app.set('view engine', 'pug');
 app.use('/client', express.static(__dirname + '/../../client/'))
 app.use('/client', express.static(__dirname + '/../../client/img'))
